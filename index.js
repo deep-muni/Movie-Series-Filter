4364c3bcad919877cc2cc1b17a117c276e2197a7
Deep Muni <deep.muni94@gmail.com>
2020-05-18 20:56:38 -0300

    Skeleton project created

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(__dirname + '/angularapp/dist/angularapp'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname + '/angularapp/dist/angularapp/index.html'));
+});
+
+app.listen(process.env.PORT || 5000, function () {
+    console.log('started');
+});
